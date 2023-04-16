abbff2306fe8cea0a696e589adf9db03851b9454
Ahmet Çetinkaya <ahmetcetinkaya7@outlook.com>
2023-04-16 19:14:37 +0300

    feat(powershell): add posh git

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -1,11 +1,40 @@
 # Aliases
-Set-Alias g git
 Set-Alias touch New-Item
-Set-Alias vim nvim
+Set-Alias g git
+Set-Alias gg gitui
 Set-Alias lg lazygit
+Set-Alias vim nvim
 
 # Prompt
 oh-my-posh init pwsh --config C:\code\config\PowerShell\ahmetcetinkaya.omp.json | Invoke-Expression
+
+# Posh Git
+$env:POSH_GIT_ENABLED = $true
+Import-Module posh-git
+
+# PSReadLine 
+Set-PSReadLineOption -PredictionSource HistoryAndPlugin
+Set-PSReadLineOption -PredictionViewStyle ListView
+Set-PSReadLineOption -BellStyle None
+Set-PSReadlineKeyHandler -Key 'Ctrl+Spacebar' -Function MenuComplete
+
+# Winget
+## Completion
+Register-ArgumentCompleter -Native -CommandName winget -ScriptBlock {
+    param($wordToComplete, $commandAst, $cursorPosition)
+        [Console]::InputEncoding = [Console]::OutputEncoding = $OutputEncoding = [System.Text.Utf8Encoding]::new()
+        $Local:word = $wordToComplete.Replace('"', '""')
+        $Local:ast = $commandAst.ToString().Replace('"', '""')
+        winget complete --word="$Local:word" --commandline "$Local:ast" --position $cursorPosition | ForEach-Object {
+            [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+        }
+}
+
+# PSFzf
+Import-Module PSFzf 
+Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory 'Ctrl+r'
+Import-Module posh-git
+
 
 # Utilities
 function which($command) {
@@ -14,12 +43,15 @@ function which($command) {
 function pubip {
 	(Invoke-WebRequest http://ifconfig.me/ip).Content
 }
-
-# PSReadLine 
-Set-PSReadLineOption -PredictionSource HistoryAndPlugin
-Set-PSReadLineOption -PredictionViewStyle ListView
-Set-PSReadLineOption -BellStyle None
-
-# PSFzf
-Import-Module PSFzf 
-Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory 'Ctrl+r'
+function del-history($command) {
+    $history = Get-History
+    $commandEntries = $history | Where-Object { $_.CommandLine -match $Command }
+    if ($commandEntries.Count -eq 0) {
+        Write-Host "No matching commands found."
+        return
+    }
+    foreach ($entry in $commandEntries) {
+        Clear-History -Id $entry.Id
+        Write-Host "Command deleted: $($entry.CommandLine)"
+    }
+}
--- powershell/README.md
+++ powershell/README.md
@@ -1,15 +1,14 @@
-# Modules
+# Tools
 
 - [Oh My Posh](https://github.com/JanDeDobbeleer/oh-my-posh)
 - [PSReadLine](https://github.com/PowerShell/PSReadLine)
 - [PSFzf](https://github.com/kelleyma49/PSFzf)
 - [Z](https://github.com/badmotorfinger/z)
 - [gsudo](https://github.com/gerardog/gsudo)
-
-# Tools
-
+- [PoshGit](https://github.com/dahlbyk/posh-git)
 - [GitUI](https://github.com/extrawurst/gitui)
 - [WinFetch](https://github.com/lptstr/winfetch)
+- [GitOpen](https://github.com/paulirish/git-open)
 
 # Apperance
 
