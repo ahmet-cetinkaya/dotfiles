e77b8a5275b4b5790b764ee1ce34b0dbdf24308c
Ahmet Çetinkaya <ahmetcetinkaya@tutamail.com>
2025-06-27 21:31:11 +0300

    fix(neovim): update autocommand comments and lazy-lock
    Comment out unused autocommands in init.lua for clarity.
    Update lazy-lock.json to reflect the latest plugin versions
    and branches for better compatibility and performance.

--- neovim/init.lua
+++ neovim/init.lua
@@ -1,12 +1,12 @@
 require("config.lazy")
 require("config.ui")
 
-vim.api.nvim_create_autocmd("VimEnter", {
-  command = "setlocal nonumber norelativenumber | ToggleTerm",
-})
-
-vim.api.nvim_create_autocmd("VimEnter", {
-  command = "setlocal nonumber norelativenumber | Neotree toggle",
-})
+-- vim.api.nvim_create_autocmd("VimEnter", {
+--   command = "setlocal nonumber norelativenumber | ToggleTerm",
+-- })
+--
+-- vim.api.nvim_create_autocmd("VimEnter", {
+--   command = "setlocal nonumber norelativenumber | Neotree toggle",
+-- })
 
 vim.opt.termguicolors = true
