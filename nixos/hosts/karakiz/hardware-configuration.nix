fdb87535e26e97785068f6de0f145d6f7931ba90
ahmet-cetinkaya <ahmetcetinkaya@tutamail.com>
2026-03-04 23:32:17 +0300

    chore(nixos): update karakiz hardware-configuration with new UUIDs and swap device
    - Regenerated hardware configuration for Karakiz NixOS host
    - Updated file system UUIDs for root and boot partitions
    - Added swap device configuration

--- nixos/hosts/karakiz/hardware-configuration.nix
+++ nixos/hosts/karakiz/hardware-configuration.nix
@@ -3,36 +3,59 @@
   lib,
   modulesPath,
   ...
-}: {
+}:
+{
   imports = [
     (modulesPath + "/installer/scan/not-detected.nix")
   ];
 
   boot = {
-    initrd.availableKernelModules = ["nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod"];
-    initrd.kernelModules = [];
-    kernelModules = ["kvm-amd"];
-    extraModulePackages = [];
+    initrd.availableKernelModules = [
+      "nvme"
+      "xhci_pci"
+      "ahci"
+      "usbhid"
+      "usb_storage"
+      "sd_mod"
+    ];
+    initrd.kernelModules = [ ];
+    kernelModules = [ "kvm-amd" ];
+    extraModulePackages = [ ];
   };
 
   fileSystems = {
     "/" = {
-      device = "/dev/disk/by-uuid/ca26f48f-62a5-41ca-a450-dec7f601ea9d";
+      device = "/dev/disk/by-uuid/16a8ee7a-960c-4ab5-a7f9-7b3c642f3e55";
       fsType = "ext4";
     };
     "/boot" = {
-      device = "/dev/disk/by-uuid/8FF4-C3C7";
+      device = "/dev/disk/by-uuid/45C2-8105";
       fsType = "vfat";
-      options = ["fmask=0077" "dmask=0077"];
+      options = [
+        "fmask=0077"
+        "dmask=0077"
+      ];
     };
+
     "/run/media/ac/hdd" = {
       device = "/dev/disk/by-uuid/68004D20004CF69A";
       fsType = "ntfs-3g";
-      options = ["auto" "nofail" "user" "exec" "uid=1000" "gid=100" "umask=022" "utf8"];
+      options = [
+        "auto"
+        "nofail"
+        "user"
+        "exec"
+        "uid=1000"
+        "gid=100"
+        "umask=022"
+        "utf8"
+      ];
     };
   };
 
-  swapDevices = [];
+  swapDevices = [
+    { device = "/dev/disk/by-uuid/55671379-832b-4ccc-af9b-b78f5c029983"; }
+  ];
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
