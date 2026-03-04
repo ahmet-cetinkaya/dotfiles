301f53b092abec1de74cf78841ea0e843fcbe276
ahmet-cetinkaya <ahmetcetinkaya@tutamail.com>
2026-03-04 23:32:48 +0300

    chore(nixos): update hytale-launcher-flatpak sha256 hash

--- nixos/pkgs/hytale-launcher-flatpak/default.nix
+++ nixos/pkgs/hytale-launcher-flatpak/default.nix
@@ -2,7 +2,7 @@
   appId = "com.hypixel.HytaleLauncher";
   bundle = builtins.fetchurl {
     url = "https://launcher.hytale.com/builds/release/linux/amd64/hytale-launcher-latest.flatpak";
-    sha256 = "sha256-hjCJuRBT7yY/KWz3M3lZLroOMSWs42dhlGt002srKgw=";
+    sha256 = "sha256-W/aVRHVaWIP43w5xucU/0Zu6JgsPfgMgLPn9M4rhLrE=";
   };
-  sha256 = "sha256-hjCJuRBT7yY/KWz3M3lZLroOMSWs42dhlGt002srKgw=";
+  sha256 = "sha256-W/aVRHVaWIP43w5xucU/0Zu6JgsPfgMgLPn9M4rhLrE=";
 }
