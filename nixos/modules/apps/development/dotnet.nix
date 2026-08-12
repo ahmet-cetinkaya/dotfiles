1fe4af8911f2b41ff538f0a9d3bffeeac29ac42f
ahmet-cetinkaya <ahmetcetinkaya.me@proton.me>
2026-08-12 20:06:49 +0300

    chore(nixos): drop insecure dotnet sdk 6

--- nixos/modules/apps/development/dotnet.nix
+++ nixos/modules/apps/development/dotnet.nix
@@ -3,17 +3,12 @@
   ...
 }: let
   dotnetSdk = pkgs.dotnetCorePackages.combinePackages [
-    pkgs.dotnetCorePackages.sdk_6_0-bin
     pkgs.dotnetCorePackages.sdk_8_0-bin
     pkgs.dotnetCorePackages.sdk_9_0-bin
     pkgs.dotnetCorePackages.sdk_10_0-bin
     pkgs.dotnetCorePackages.sdk_11_0-bin
   ];
 in {
-  nixpkgs.config.permittedInsecurePackages = [
-    "dotnet-sdk-6.0.428"
-  ];
-
   environment.systemPackages = with pkgs; [
     dotnetSdk # Keep multiple SDK generations available for projects with different targets.
     csharpier
